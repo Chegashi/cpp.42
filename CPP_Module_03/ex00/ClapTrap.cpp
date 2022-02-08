a9a2f080a3b0d7cf287b498c0f178378b8952cfd
Mohamed Chegri <mochegri@e2r4p8.1337.ma>
2022-02-08 17:21:56 +0100

    Undefined ClapTrap::claps

--- CPP_Module_03/ex00/ClapTrap.cpp
+++ CPP_Module_03/ex00/ClapTrap.cpp
@@ -6,27 +6,26 @@
 /*   By: mochegri <mochegri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 18:34:42 by mochegri          #+#    #+#             */
-/*   Updated: 2022/02/07 23:51:44 by mochegri         ###   ########.fr       */
+/*   Updated: 2022/02/08 17:19:28 by mochegri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include <iostream>
 
-#define CLAPS_NBR  10 
-
-ClapTrap		claps[CLAPS_NBR];
 int				ClapTrap::nbr_claps = 0;
 
-void	ft_save(ClapTrap &src)
+void	ClapTrap::ft_save(ClapTrap &src)
 {
-	claps[ClapTrap::nbr_claps++] = src;
-	std::cout << ClapTrap::nbr_claps << std::endl;
+	if (ClapTrap::nbr_claps < NBR_CLAPS_MAX)
+		ClapTrap::claps[(ClapTrap::nbr_claps)++] = src;
+	else
+		std::cout << "you can't have more than:" << ClapTrap::nbr_claps << "CLAPS" << std::endl;
 }
 
 ClapTrap::ClapTrap() : name(""), hit_point(10), energy_point(10), attack_damage(0)
 {
-	ft_save(*this);
+	ClapTrap::ft_save(*this);
 }
 
 ClapTrap::~ClapTrap()
@@ -36,25 +35,25 @@ ClapTrap::~ClapTrap()
 
 ClapTrap::ClapTrap(std::string name) : name(name), hit_point(10), energy_point(10), attack_damage(0)
 {
-	ft_save(*this);
+	ClapTrap::ft_save(*this);
 }
 
 ClapTrap::ClapTrap(ClapTrap &src)
 {
 	*this = src;
-	ft_save(*this);
+	ClapTrap::ft_save(*this);
 }
 
 void	ClapTrap::attack(const std::string& target)
 {
 	int j;
 
-	for(j = 0; j < CLAPS_NBR && target == claps[j].name && target != ""; j++)
+	for(j = 0; j < ClapTrap::nbr_claps && target == ClapTrap::claps[j].name && target != ""; j++)
 		;
-	if (j == CLAPS_NBR)
+	if (j == ClapTrap::nbr_claps)
 		std::cout << target << "clapsTrap Not found" << std::endl;
 	else
-		claps[j].takeDamage(this->attack_damage);
+		ClapTrap::claps[j].takeDamage(this->attack_damage);
 }
 
 void	ClapTrap::takeDamage(unsigned int amount)
--- CPP_Module_03/ex00/ClapTrap.hpp
+++ CPP_Module_03/ex00/ClapTrap.hpp
@@ -6,14 +6,14 @@
 /*   By: mochegri <mochegri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 18:34:38 by mochegri          #+#    #+#             */
-/*   Updated: 2022/02/07 23:50:29 by mochegri         ###   ########.fr       */
+/*   Updated: 2022/02/08 17:18:51 by mochegri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _ClapTrap_H
 #define _ClapTrap_H
 #include <string>
-
+#define NBR_CLAPS_MAX 5
 class ClapTrap
 {
 	public:
@@ -23,9 +23,11 @@ class ClapTrap
 		~ClapTrap();
 
 		static int	nbr_claps;
+		static ClapTrap claps[NBR_CLAPS_MAX];
 		void attack(const std::string& target);
 		void takeDamage(unsigned int amount);
 		void beRepaired(unsigned int amount);
+		static void	ft_save(ClapTrap &src);
 
 	private:
 		std::string	name;
@@ -33,4 +35,6 @@ class ClapTrap
 		int			energy_point;
 		int			attack_damage;
 };
+
+
 #endif
