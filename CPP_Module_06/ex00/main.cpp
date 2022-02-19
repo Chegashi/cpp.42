33f8d087035b4b2a2fac475165ddf8ca2ad83d14
chegashi <mochegri@student.1337.ma>
2022-02-19 05:21:02 +0100

    init d06

---
+++ CPP_Module_06/ex00/main.cpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mochegri <mochegri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 16:39:25 by mochegri          #+#    #+#             */
+/*   Updated: 2022/02/19 05:19:09 by mochegri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <typeinfo>
+#include <iostream>
+#include <ctype.h>
+
+// bool	is_int(char *arg)
+// {
+// 	while(*arg)
+// 		if (!isalpha(*arg++))
+// 			return (false);
+// 	return (true);
+// }
+
+void	to_char(std::string arg)
+{
+	try
+	{
+		int n = std::stoi(arg);
+		if (isascii(n))
+			std::cout << "char: " << static_cast<char>(n) << std::endl;
+		else
+			std::cout << "char: impossible" << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "char: " << arg << std::endl;
+	}
+}
+
+void	to_int(std::string arg)
+{
+	try
+	{
+		int n = std::stoi(arg);
+		std::cout << "int: " << static_cast<int>(n) << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "int: impossible" << std::endl;
+	}
+}
+
+void	to_float(std::string arg)
+{
+	try
+	{
+		float n = std::stof(arg);
+		float f = static_cast<float>(n);
+		std::cout << "float: " << f << (f - (int)f ? "" : ".0") << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "float: impossible" << std::endl;
+	}
+}
+
+void	to_dooble(std::string arg)
+{
+	try
+	{
+		double n = std::stod(arg);
+		double d = static_cast<double>(n);
+		std::cout << "double: " << d << (d - (int)d ? "" : ".0") << "f" << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "double: impossible" << std::endl;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac !=2 )
+	{
+		std::cerr << "the conversion is impossible" << std::endl;
+		return (0);
+	}
+	else
+	{
+		std::string arg = av[1];
+		// std::cout << arg << std::endl;
+		to_char(arg);
+		to_int(arg);
+		to_float(arg);
+		to_dooble(arg);
+	}
+
+	return (0);
+}
