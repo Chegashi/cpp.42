64b0d82f9bc0052490fa6fbd38af3b759e00c391
chegashi <mochegri@student.1337.ma>
2022-02-21 18:27:09 +0100

    finish d07

--- CPP_Module_07/ex02/Array.hpp
+++ CPP_Module_07/ex02/Array.hpp
@@ -6,27 +6,65 @@
 /*   By: mochegri <mochegri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 04:50:24 by mochegri          #+#    #+#             */
-/*   Updated: 2022/02/21 05:31:14 by mochegri         ###   ########.fr       */
+/*   Updated: 2022/02/21 18:25:55 by mochegri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_H
 #define ARRAY_H
+#include <iostream>
+#include <cstdlib>
 
 template <typename T>
 
 class Array
 {
     private:
-
+        T *array;
+        unsigned int len;
+    class InvalidIndexException : public std::exception
+    {
+        public:
+            virtual const char * what() const throw ()
+            {
+                return "bad index";
+            }
+    };
     public:
-        Array();
-        Array(unsigned int n);
-        ~Array();
-        operator=(const Array &src);
+        Array<T>() : len(0)
+        {
+            array = NULL;
+        }
+
+        Array<T>(unsigned int n) : len(n)
+        {
+            array = new T[n];
+        }
+
+        ~Array()
+        {
+            
+        }
+
+        T & operator[](const unsigned int &index)
+        {
+            if (index >= this->len)
+                throw Array::InvalidIndexException();
+            return (this->array[index]);
+        }
+
+        Array operator=(const Array &src)
+        {
+            if (this->len)
+                delete this->array;
+            this->array = new T [src.len];
+            for (int i ; i < src.size(); i++)
+                this->array[i] = src.array[i];
+            return(*this);
+        }
+        unsigned int size() const
+        {
+            return this->len;
+        }
 };
-// Array<int>::Array(int)
-// {
-    
-// }
 #endif
--- CPP_Module_07/ex02/main.cpp
+++ CPP_Module_07/ex02/main.cpp
@@ -5,49 +5,54 @@
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
-    // int* mirror = new int[MAX_VAL];
-    // srand(time(NULL));
-    // for (int i = 0; i < MAX_VAL; i++)
-    // {
-    //     const int value = rand();
-    //     numbers[i] = value;
-    //     mirror[i] = value;
-    // }
-    // //SCOPE
-    // {
-    //     Array<int> tmp = numbers;
-    //     Array<int> test(tmp);
-    // }
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+        for (int i = 0; i < MAX_VAL; i++)
+    {
+        std::cout << numbers[i] << std::endl;
+    }
 
-    // for (int i = 0; i < MAX_VAL; i++)
-    // {
-    //     if (mirror[i] != numbers[i])
-    //     {
-    //         std::cerr << "didn't save the same value!!" << std::endl;
-    //         return 1;
-    //     }
-    // }
-    // try
-    // {
-    //     numbers[-2] = 0;
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
-    // try
-    // {
-    //     numbers[MAX_VAL] = 0;
-    // }
-    // catch(const std::exception& e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
-    // for (int i = 0; i < MAX_VAL; i++)
-    // {
-    //     numbers[i] = rand();
-    // }
-    // delete [] mirror;//
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
     return 0;
 }
