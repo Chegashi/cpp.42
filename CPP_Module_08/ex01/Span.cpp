39d618eb3a379485adb236e61926197894555041
chegashi <mochegri@student.1337.ma>
2022-02-22 23:27:08 +0100

    add ex01

--- CPP_Module_08/ex01/Span.cpp
+++ CPP_Module_08/ex01/Span.cpp
@@ -6,24 +6,39 @@
 /*   By: mochegri <mochegri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 20:46:37 by mochegri          #+#    #+#             */
-/*   Updated: 2022/02/22 23:06:24 by mochegri         ###   ########.fr       */
+/*   Updated: 2022/02/22 23:25:37 by mochegri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 
 Span::Span(unsigned int n) : len(n), index(0)
 {
-	;
 
+}
+
+Span::Span(const Span &src) : len(src.len), index(src.index)
+{
+	this->g = src.g;
 }
 
 Span::~Span()
 {
 	
 }
+Span & Span::operator=(Span &rhs)
+{
+	this->len = rhs.len;
+	this->index = rhs.len;
+	this->g.clear();
+	for (std::vector<int>::iterator it = rhs.g.begin(); it ==rhs.g.end(); it++)
+		this->g.push_back(*it);
+	return (*this);
+}
+
 void	Span::addNumber(int n)
 {
 	if (this->index < this->len)
--- CPP_Module_08/ex01/Span.hpp
+++ CPP_Module_08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: mochegri <mochegri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 20:45:38 by mochegri          #+#    #+#             */
-/*   Updated: 2022/02/22 23:05:53 by mochegri         ###   ########.fr       */
+/*   Updated: 2022/02/22 23:23:41 by mochegri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,20 +33,20 @@ class Span
 	};
 	public:
 		Span(unsigned int n);
+		Span(const Span &src);
 		~Span();
 
 		void	addNumber(int n);
 		int		shortestSpan();
 		int		longestSpan();
-
+		Span & operator=(Span &rhs);
 	private:
 		std::vector<int> g;
-		const int	len;
-		int			index;
-		int		short_s;
-		int			longe_s;
+		int	len;
+		int	index;
+		int	short_s;
+		int longe_s;
 		Span();
-
 };
 
 #endif
